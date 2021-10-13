b51485ac4d73882928fca3b32b125efaf0250073
samir <saitsamir@hotmail.fr>
2021-10-13 19:33:36 +0200

    maj2

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,14 +1,3 @@
-#require 'dotenv'# Appelle la gem Dotenv
-#
-#Dotenv.load('.env') # Ceci appelle le fichier .env (situé #dans le même dossier que celui d'où tu exécute app.rb)
-## et grâce à la gem Dotenv, on importe toutes les données #enregistrées dans un hash ENV
-#
-## Il est ensuite très facile d'appeler les données du hash #ENV, par exemple là je vais afficher le contenu de la clé #TWITTER_API_SECRET
-#puts ENV['TWITTER_API_SECRET']
-#
-##Autre exemple 
-#puts ENV['BEST_WEBSITE_EVER']
-
 
  # ligne très importante qui appelle la gem.
  require 'twitter'
@@ -27,4 +16,13 @@
  
  # ligne qui permet de tweeter sur ton compte
  client.update('my first tweet in Ruby !!!!')
+# tweeter des truc sur le twitter des gens 
+  
+  tweetcompt = ["@frontendmenteur", "@luuucasog","@Ragnvulf", "@8UK0W5K1", "charleedouard1", "@mrprst", "@L_ChamCham", "@lamour_ronan", "@bigdduwa", "@nathvideobot"]
 
+  random = tweetcompt.sample(5)
+
+  random.each do |current|
+    client.update("Bonjour monde ceci est un tweet automatique  #{current}.")
+     puts current
+  end
